564222b710816ba8b38c66342898bbbbb47e7dae
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-10-20 09:52:44 -0700

    notes

--- fetch-requests.js
+++ fetch-requests.js
@@ -5,16 +5,62 @@ Paste your code for fetch requests here once you finish each phase.
 
 /* ============================== Phase 1 ============================== */
 
-// Your code here
+const reqOptions = {
+  method: 'GET'
+}
 
 
+  fetch("/products", {
+    method: 'POST',
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery", 
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
 
 /* ============================== Phase 2 ============================== */
 
-// Your code here
+fetch("/products", {
+  method: 'POST',
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery", 
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+}).then((res) => {
+    console.log(res.status);
+    console.log(res.ok);
+    console.log(res.url); 
+})
 
+// [[Prototype]]: Promise[[PromiseState]]: "fulfilled"[[PromiseResult]]: undefined
+// 200
+// true
 
 
 /* ============================== Phase 3 ============================== */
+
+let params = new URLSearchParams({
+  name: "Caribbean Delight Coffee",
+  description: "Made by Manatee Coffee",
+  price: 11.99,
+  categories: "grocery"
+});
+
+
+
+fetch("/products", {
+  method: 'POST',
+  body: params, 
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+}).then((res) => {
+    console.log(res.status);
+    console.log(res.ok);
+    console.log(res.url); 
+    console.log(res.text()); 
+})
+
+
 
 // Your code here
